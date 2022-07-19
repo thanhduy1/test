768ce9f818ae562698153027c0554019ff4a6ad2
thanhduy1 <tranthanhduytvh10@gmail.com>
2022-07-19 08:29:47 +0700

    add

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
-    $server_name = "localhost";
-    $username = "root";
-    $password = "";
-    $database_name = "jolibee";
+    $server_name = "sql3.freemysqlhosting.net";
+    $username = "sql3507377";
+    $password = "2QfnCRWGXK";
+    $database_name = "sql3507377";
     $conn = mysqli_connect($server_name, $username, $password, $database_name);
     if (!$conn) {
             echo "Error " ;
